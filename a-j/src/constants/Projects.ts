93de328489f441f62a258d8bd0431fa4c4343a95
harfore <remiharfoush@gmail.com>
2026-04-13 13:39:32 +0200

    image rework

--- a-j/src/constants/Projects.ts
+++ a-j/src/constants/Projects.ts
@@ -1,11 +1,65 @@
+import page2 from '../assets/images/page2.png';
+import page3 from '../assets/images/page3.png';
+import page4 from '../assets/images/page4.png';
+import page5 from '../assets/images/page5.png';
+import page6 from '../assets/images/page6.png';
+import page7 from '../assets/images/page7.png';
+import page8 from '../assets/images/page8.png';
+import page9 from '../assets/images/page9.png';
+import page10 from '../assets/images/page10.png';
+import page11 from '../assets/images/page11.png';
+import page12 from '../assets/images/page12.png';
+import page13 from '../assets/images/page13.png';
+import page14 from '../assets/images/page14.png';
+import page15 from '../assets/images/page15.png';
+import page16 from '../assets/images/page16.png';
+import page17 from '../assets/images/page17.png';
+import page18 from '../assets/images/page18.png';
+import page19 from '../assets/images/page19.png';
+import page20 from '../assets/images/page20.png';
+import page21 from '../assets/images/page21.png';
+import page22 from '../assets/images/page22.png';
+import page23 from '../assets/images/page23.png';
+import page24 from '../assets/images/page24.png';
+import page25 from '../assets/images/page25.png';
+import page26 from '../assets/images/page26.png';
+import page27 from '../assets/images/page27.jpeg';
+import page28 from '../assets/images/page28.jpeg';
+import page29 from '../assets/images/page29.jpeg';
+import page30 from '../assets/images/page30.jpg';
+import page31 from '../assets/images/page31.jpeg';
+import page32 from '../assets/images/page32.jpg';
+import page33 from '../assets/images/page33.jpeg';
+import page34 from '../assets/images/page34.jpeg';
+import page35 from '../assets/images/page35.jpeg';
+import page36 from '../assets/images/page36.jpeg';
+import page37 from '../assets/images/page37.jpeg';
+import page38 from '../assets/images/page38.jpeg';
+import page39 from '../assets/images/page39.jpeg';
+import page40 from '../assets/images/page40.jpeg';
+import page41 from '../assets/images/page41.jpg';
+import page42 from '../assets/images/page42.jpg';
+import page43 from '../assets/images/page43.jpg';
+import page44 from '../assets/images/page44.jpg';
+import page45 from '../assets/images/page45.jpg';
+import page46 from '../assets/images/page46.jpg';
+import page47 from '../assets/images/page47.jpg';
+import page48 from '../assets/images/page48.jpg';
+import page49 from '../assets/images/page49.jpeg';
+import page50 from '../assets/images/page50.jpeg';
+import page51 from '../assets/images/page51.jpeg';
+import page52 from '../assets/images/page52.jpeg';
+import page53 from '../assets/images/page53.jpeg';
+
+
 const projects = [
     {
         index: 0,
         title: "PACK YOUR BODY",
         images: [
-            { src: "/images/page2.png" },
-            { src: "/images/page3.png" },
-            { src: "/images/page4.png" },
+            { src: page2 },
+            { src: page3 },
+            { src: page4 },
         ],
         description: "Design, création et direction artistique. Vêtements inspirés d'emballage en carton, de corps et de dimensions, transformé en vêtements. Ce projet est réalisé en laine bouillie.",
     },
@@ -13,8 +67,8 @@ const projects = [
         index: 1,
         title: "LE VOILE DU DEUIL",
         images: [
-            { src: "/images/page5.png" },
-            { src: "/images/page6.png" },
+            { src: page5 },
+            { src: page6 },
         ],
         description: "Design, création, direction artistique et photographie. Projet de guimpe réalisé dans un workshop de knitwear. Inspiré de l'artiste Majd Abdel Hamide qui travaille la borderie et qui cherche sa propre manière de faire le deuil.",
     },
@@ -22,9 +76,9 @@ const projects = [
         index: 2,
         title: "THE FLYING COAT",
         images: [
-            { src: "/images/page7.png" },
-            { src: "/images/page8.png" },
-            { src: "/images/page9.png" },
+            { src: page7 },
+            { src: page8 },
+            { src: page9 },
         ],
         description: "Design, création et direction artistique. Projet inspiré des tenues des danseurs de Soufie, de la guerre et du temps. Ce projet est réalisé en laine et en polyester.",
     },
@@ -32,8 +86,8 @@ const projects = [
         index: 3,
         title: "THE GRAPHIC SHIRT",
         images: [
-            { src: "/images/page10.png" },
-            { src: "/images/page11.png" },
+            { src: page10 },
+            { src: page11 },
         ],
         description: "Design, création, direction artistique et photographique. Projet réalisé dans le but de trouver une harmonie, et de créer un vêtement avec des motifs de rayures différents. Ce vêtement est créé à partir de trois tissus en polyester et en coton. Le shooting photo a eu lieu à l'espace Monte-Cristo, devant l'oeuvre de Manurel Merida intitulée Ordre et désordre. Une vidéo a été produite à l'exposition de ce projet en collaboration avec Hélène Dinke.",
 
@@ -42,12 +96,12 @@ const projects = [
         index: 4,
         title: "LN MAGAZINE",
         images: [
-            { src: "/images/page12.png" },
-            { src: "/images/page13.png" },
-            { src: "/images/page14.png" },
-            { src: "/images/page15.png" },
-            { src: "/images/page16.png" },
-            { src: "/images/page17.png" }
+            { src: page12 },
+            { src: page13 },
+            { src: page14 },
+            { src: page15 },
+            { src: page16 },
+            { src: page17 },
         ],
         description: "Travail au but de montrer mon univers et mes inspirations quotidiennes, et de développement des compétences sur indesign."
     },
@@ -55,9 +109,9 @@ const projects = [
         index: 5,
         title: "THE SILHOUETTES",
         images: [
-            { src: "/images/page18.png" },
-            { src: "/images/page19.png" },
-            { src: "/images/page20.png" }
+            { src: page18 },
+            { src: page19 },
+            { src: page20 },
         ],
         description: "Travail réalisé à partir de 16 silhouettes. Au but d'apprendre à chercher des vêtements de seconde main et de savoir les habiller au quotidien. Avec un vêtement upcyclé."
     },
@@ -65,9 +119,9 @@ const projects = [
         index: 6,
         title: "PROJECT 7",
         images: [
-            { src: "/images/page21.png" },
-            { src: "/images/page22.png" },
-            { src: "/images/page23.png" }
+            { src: page21 },
+            { src: page22 },
+            { src: page23 }
         ],
         description: "Traitement d'un sujet sur les matières premières. La première image est nature morte minimale composée d'une assiette en argile blanche, d'un amas de farine et d'un verre d'eau croupie. J'ai voulu dénoncer les conditions de vie des victimes de guerre. Malgré tout le confort que l'on a pu avoir avant le conflit, la guerre nous a renvoyé à quelque chose de plus archaïque. Dans la matière première, je vois un retour à l'état primitif. Avant le beurre existait le lait et avant le pain existait la farine. L'assiette est l'allégorie de cette conditions humaine, j'ai opté pour l'argile car dans les croyances islamiques, il est dit qu'Adam a été crée d'un mélange d'argiles. La perforation en son centre symoblise un manque qui est nécessaire à la stabilité de tout être humain, la matière première pour un esprit sain : la sécurité."
     },
@@ -75,18 +129,18 @@ const projects = [
         index: 7,
         title: "INTEGRITY",
         images: [
-            { src: "/images/page24.png" },
-            { src: "/images/page25.png" },
-            { src: "/images/page26.png" }
+            { src: page24 },
+            { src: page25 },
+            { src: page26 }
         ],
         description: "J'ai voulu travailler autour du vêtement. J'ai pris un gilet en maille de production industrielle que j'ai détérioré puis dé-filé. Tout d'abord j'ai voulu revenir sur le concept de sécurité abordé plus haut. Après la sécurité psychique, j'ai voulu illustrer la sécurité physique car le vêtement n'est pas qu'objet d'apparat ou de distinction sociale. J'ai souhaité revenir à la nature première d'un vêtement : l'intégrité physique. S'habiller pour se protéger. Cependant, on peut également le voir sous le spectre du vêtement 'contenant', du vêtement réconfortant. Un pull que notre défunt père aurait pu laisser sur les ruines de la maison familiale par exemple. Implicitement, au travers de ce processus j'ai souhaité inverser le système de production. Au lieu d'utiliser une matière première pour la transformer en objet, j'ai voulu transformer l'objet en matière. On revient sur ce concept de changement d'état / changement de condition. La matière et l'humain ne font plus qu'un."
     },
     {
         index: 8,
-        title: "Polaroids",
+        title: "POLAROIDS",
         images: [
-            { src: "/images/page27.jpeg" },
-            { src: "/images/page28.jpeg" }
+            { src: page27 },
+            { src: page28 }
         ],
         description: " "
     },
@@ -94,10 +148,10 @@ const projects = [
         index: 9,
         title: "",
         images: [
-            { src: "/images/page29.jpeg" },
-            { src: "/images/page30.jpg" },
-            { src: "/images/page31.jpeg" },
-            { src: "/images/page32.jpg" },
+            { src: page29 },
+            { src: page30 },
+            { src: page31 },
+            { src: page32 },
         ],
         description: " "
     },
@@ -105,57 +159,55 @@ const projects = [
         index: 10,
         title: "",
         images: [
-            { src: "/images/page33.jpeg" },
-            { src: "/images/page34.jpeg" },
-            { src: "/images/page35.jpeg" }
+            { src: page33 },
+            { src: page34 },
+            { src: page35 }
         ],
         description: " "
     },
     {
         index: 11,
-        title: "Citizen Watch",
+        title: "CITIZEN WATCH",
         images: [
-            { src: "/images/page36.jpeg" },
-            { src: "/images/page37.jpeg" },
-            { src: "/images/page38.jpeg" },
-            { src: "/images/page39.jpeg" },
-            { src: "/images/page40.jpeg" },
-            { src: "/images/page41.jpg" }
+            { src: page36 },
+            { src: page37 },
+            { src: page38 },
+            { src: page39 },
+            { src: page40 },
+            { src: page41 }
         ],
         description: " "
     },
     {
         index: 12,
-        title: "Citizen Press Day",
+        title: "CITIZEN PRESS DAY",
         images: [
-            { src: "/images/page42.jpg" },
-            { src: "/images/page43.jpg" },
-            { src: "/images/page44.jpg" },
-            { src: "/images/page45.jpg" },
-            { src: "/images/page46.jpg" },
-            { src: "/images/page47.jpg" },
+            { src: page42 },
+            { src: page43 },
+            { src: page44 },
+            { src: page45 },
+            { src: page46 },
+            { src: page47 },
         ],
     },
     {
         index: 13,
-        title: "Super Titanium Event",
+        title: "SUPER TITANIUM EVENT",
         images: [
-            { src: "/images/page48.jpg" },
-            { src: "/images/page49.jpeg" },
-            { src: "/images/page50.jpeg" },
-            { src: "/images/page51.jpeg" },
+            { src: page48 },
+            { src: page49 },
+            { src: page50 },
+            { src: page51 },
         ],
     },
     {
         index: 14,
         title: "",
         images: [
-            { src: "/images/page52.jpeg" },
-            { src: "/images/page53.jpeg" },
+            { src: page52 },
+            { src: page53 },
         ]
     }
-
-
-]
+];
 
 export default projects;
